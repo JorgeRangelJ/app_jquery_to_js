de33541726973fd51588acc980bf35f2067fdac8
Jorge Rangel <rangel2016sistemas@gmail.com>
2018-11-30 12:51:25 -0500

    Creación de elementos y asignación de atributos

--- src/js/home.js
+++ src/js/home.js
@@ -110,10 +110,24 @@ fetch('https://randomuser.me/api/') // fectch Se usa para pedir datos de un serv
 
     const $form = document.getElementById('form')
     const $home = document.getElementById('home')
+    const $featuringContainer = document.getElementById('featuring')
+
+    function setAttributes($element, $attributes) {
+      for (const attribute in attributes) {
+        $element.setAttributes(attribute, attributes[attribute]);
+      }
+    }
 
     $form.addEventListener('submit', (event) => {
       event.preventDefault();
       $home.classList.add('search-active')
+      const $loader = document.createElement('img');
+      setAttributes($loader, {
+        src: 'src/images/loader.gif',
+        height: 50,
+        width: 50,
+      })
+      $featuringContainer.append($loader);
     })
 
     const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
@@ -159,8 +173,6 @@ fetch('https://randomuser.me/api/') // fectch Se usa para pedir datos de un serv
       });
     }
 
-
-
     const $actionContainer = document.querySelector('#action')
     renderMovieList(actionList.data.movies, $actionContainer)
     
@@ -169,14 +181,6 @@ fetch('https://randomuser.me/api/') // fectch Se usa para pedir datos de un serv
 
     const $animationContainer = document.getElementById('animation')
     renderMovieList(animationList.data.movies, $animationContainer)
-
-
-
-    // Selector para los featuring
-    const $featuringContainer = document.getElementById('#featuring')
-
-
-    
 
     // Selectores para el modal
     const $modal = document.getElementById('modal');
